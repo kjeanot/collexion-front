8fa8399251673d407f82a2e0fa2ec13d437412bc
kjeanot <kevin.jeanot@gmail.com>
2024-02-06 12:12:51 +0100

    install tailwind dependencies

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>
+        <p className="bg-red-700">
           Edit <code>src/components/App/App.tsx</code> and save to reload.
         </p>
 
