00afc13ee7eae2c92cbf78673a1efcd68a1509b5
kjeanot <kevin.jeanot@gmail.com>
2024-02-08 15:18:05 +0100

    feat : CollectionCTA component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo.svg';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { fetchCollections } from '../../store/reducers/collections';
 import CollectionsList from "../Collection/CollectionsList";
+import CollectionCTA from "../Collection/CollectionCTA";
 import Footer from '../Footer/Footer';
 import Gallery from '../Gallery/Gallery';
 import Header from '../Header/Header';
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="App container mx-auto px-4 max-w-screen-xl">
       <Header />
+      <CollectionCTA />
       <CollectionsList data={collectionList} />
       <Footer />
     </div>
--- src/store/reducers/collections.ts
+++ src/store/reducers/collections.ts
@@ -11,107 +11,14 @@ interface CollectionsState {
 }
 
 export const initialState: CollectionsState = {
-  list: [
-    {
-      id: 2,
-      name: 'hrthsrths',
-      image: 'hsrthsrth',
-      description: 'thsrthsrthszrth',
-      rating: 5,
-    },
-    {
-      id: 3,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 4,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 5,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 6,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '5 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 7,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '6 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 8,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '7 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 9,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '8 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 10,
-      name: 'Ma premiere collection ',
-      image: 'https://via.placeholder.com/150',
-      description:
-        '9 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Pretium lectus quam id leo in vitae turpis massa. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Ac turpis egestas integer eget aliquet nibh praesent tristique. ',
-      rating: null,
-    },
-    {
-      id: 11,
-      name: 'Name',
-      image: 'Image',
-      description: 'Description',
-      rating: 3,
-    },
-    {
-      id: 12,
-      name: 'Name',
-      image: 'Image',
-      description: 'Description',
-      rating: 3,
-    },
-    {
-      id: 13,
-      name: 'Name',
-      image: 'Image',
-      description: 'Description',
-      rating: 3,
-    },
-  ],
+  list: [],
 };
 
 export const fetchCollections = createAsyncThunk(
   'collections/fetchCollections',
   async (_, thunkAPI) => {
     const response = await axios.get(
-      'http://64ed31429cbded49acab4281.cloud.lan/Apothéose/collexion/projet-12-collexion-back/public/api/collections'
+      'http://64ed31429cbded49acab4281.cloud.lan/Apothéose/collexion/projet-12-collexion-back/public/api/collections',
     );
     return response.data;
   }
