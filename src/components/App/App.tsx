ea2cbe2aed762575c828038e86a15e2595a4a188
Andy-Bevis <143426867+Andy-Bevis@users.noreply.github.com>
2024-02-09 14:44:30 +0100

    component/subscribe_finished

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,22 @@
-import logo from "../../assets/logo.svg";
-import Card from "../Card/Card";
-import Footer from "../Footer/Footer";
-import Gallery from "../Gallery/Gallery";
-import Header from "../Header/Header";
-import Carroussel from "../Carroussel/Carroussel"
-import "./App.scss";
-import store from "../../store";
+import logo from '../../assets/logo.svg';
+import Card from '../Card/Card';
+import Footer from '../Footer/Footer';
+import Gallery from '../Gallery/Gallery';
+import Header from '../Header/Header';
+import Carroussel from '../Carrousel/Carrousel';
+import './App.scss';
+import store from '../../store';
+import Subscribe from '../Subscribe/Subscribe';
+import Content from '../Content/Content';
 
 function App() {
   return (
     <div className="App container mx-auto px-4 max-w-screen-xl">
       <Header />
+      {/* <Content /> */}
+      <Subscribe />
+      {/* <Carroussel /> */}
+      {/* <Gallery /> */}
       <Footer />
     </div>
   );
